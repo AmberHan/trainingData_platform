462fd3fe9608378024201ec738d4788ee584739d
杨冬晗 <1198391037@qq.com>
2024-10-15 21:43:33 +0800

    fix

--- services/file/file_service.py
+++ services/file/file_service.py
@@ -50,6 +50,7 @@ def download_impl(
         req: DownloadParams
 ):
     file_location = ""
+    add = False
     if req.workId is not None:
         work_path = f".{config.RUNS_HELMET_PATH}/{req.workId}"
         train_count = count_directories(work_path, "train1") * '1'
@@ -58,17 +59,26 @@ def download_impl(
         file_location = f"{config.config_path['PathConf']['SaveModelPath']}/{req.modelName}.pt"
     elif req.path is not None:
         file_location = f"{config.config_path['PathConf']['SaveDataPath']}/{req.path}"
-    return download_file(file_location)
+        add = True
+    return download_file(file_location, add)
 
 
-def download_file(path: str):
+def download_file(path: str, add: bool = False):
+    # 检查路径是否安全
     safe_path = Path(path).resolve()
     if not safe_path.is_file() or not safe_path.exists():
         raise HTTPException(status_code=404, detail="File not found")
 
+    # 获取文件的基本名称
     filename = os.path.basename(safe_path)
-    headers = {
-        "Content-Disposition": f"inline; filename={filename}",
-        "Content-Length": str(safe_path.stat().st_size),
-    }
+
+    # 设置响应头
+    headers = {}
+    if add:
+        headers = {
+            "Content-Disposition": f"inline; filename={filename}",
+            "Content-Length": str(safe_path.stat().st_size),
+        }
+
+    # 返回文件
     return FileResponse(path=str(safe_path), filename=filename, headers=headers)
