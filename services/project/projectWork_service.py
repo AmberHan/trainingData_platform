e4615104ca7122a7abcb0d94a345f59d3a231f4b
wenjiePan <940731286@qq.com>
2024-10-15 10:48:04 +0800

    stop修正

--- services/project/projectWork_service.py
+++ services/project/projectWork_service.py
@@ -384,7 +384,7 @@ def stop_work(req: StringIdReq, db: Session):
     if work.WorkStatus == 2:
         raise Exception("任务暂未开启！")
     else:
-        work.WorkStatus = 2
+        work.WorkStatus = 1
         work.save(db)
     # 关闭pid
     # 设置配置文件路径 TODO 设置动态从前端获取
