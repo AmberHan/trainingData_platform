f66d3f1a0eb32a6eeff0968f8feb190e88f0220d
杨冬晗 <1198391037@qq.com>
2024-10-15 09:01:07 +0800

    Revert "f"
    This reverts commit d90a7875608d65c49699b2171a0f4c6a7d50b384.

--- router/user_router.py
+++ router/user_router.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from starlette.staticfiles import StaticFiles
 
 from common import const
-from config.config import config_path
 from config.db_config import get_db
 from schemas.user_model import UserLoginReq
 from services.user.user_service import user_login_impl
@@ -11,7 +9,6 @@ from util.response_format import response_format
 
 userHandle = APIRouter(prefix=const.API_URL_PREFIX + "/api-u")
 
-userHandle.mount("/static", StaticFiles(directory=config_path['PathConf']['SaveDataPath']), name="static")
 
 # 用户管理模块
 @userHandle.post("/login")
